46a2aaa8fb65478435d1e3c45070fd72efe748ea
Tejasree-S27 <stejasree348@gmail.com>
2024-12-10 19:54:37 +0530

    Add files via upload

---
+++ StringUsingFOrEach.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class StringUsingFOrEach 
+{
+  public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     String a[]={"apple","orange","mango"};
+     for(String b:a)
+     System.out.print(b+",");
+     
+  }
+}
