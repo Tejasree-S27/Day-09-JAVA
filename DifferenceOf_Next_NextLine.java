a576fe8eeebfa5a199e76882b6ca0975dc1fa102
Tejasree-S27 <stejasree348@gmail.com>
2024-12-10 18:59:36 +0530

    Add files via upload

---
+++ DifferenceOf_Next_NextLine.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class DifferenceOf_Next_NextLine
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    //using nextLine()
+    String a=sc.nextLine();
+    System.out.println(a);
+    //using next()
+    String b=sc.next();
+    System.out.println(b);
+    
+  }
+}
